d03be6a0311291776b4f1d6c5cf8eded538b7214
Daniel Ayala <danielayala8@gmail.com>
2025-10-02 16:43:21 -0500

    Se agrego el metodo post con exito

--- controllers/libroController.js
+++ controllers/libroController.js
@@ -2,7 +2,29 @@
 const db = require('../config/db')
 
 exports.createLibro = async(req, res) =>{
-    console.log('Has ejecutado POST CHAVAL')
+    // Obtenemos los datos del body
+    const {titulo, autor, numpaginas, categoria} = req.body
+    
+    //Preparamos la consulta SQL
+    const sql = "INSERT INTO libros(titulo, autor, numpaginas, categoria) VALUES(?, ?, ?, ?)"
+
+    //Validamos que no falte ningún dato
+    if(!titulo || !autor || !numpaginas || !categoria){
+        return res.status(400).json({error: 'No se permiten valores vacios'})
+    }
+
+    try {
+        //Ejecutamos la consulta SQL
+        const [result] = await db.query(sql, [titulo, autor, numpaginas, categoria])
+        
+        //Devolvemos una respuesta de exito
+        res.status(201).json({ 
+            id: result.insertId,
+            mensaje: 'Se agrego el libro'})
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({error: 'Error en el servidor'})
+    }
 }
 
 exports.getLibro = async(req, res) =>{
@@ -10,7 +32,26 @@ exports.getLibro = async(req, res) =>{
     try {
         //Obtenemos solo los dato
         const [libros] = await db.query(sql)
-        res.status(200).json({libros})    
+        return res.status(200).json({libros})    
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({mensaje: 'Error en el servidor'})
+    }
+}
+
+exports.getLibrobyId = async(req, res) =>{
+    const {id} = req.params
+    const sql = "SELECT id, titulo, autor, numpaginas, categoria FROM libros WHERE id = ?"
+    try {
+        //Obtenemos solo los datos
+        const [libros] = await db.query(sql, [id])
+
+        //Enviamos un mensaje de error en caso de no encontrar el libro
+        if (!libros.length) {
+            return res.status(404).json({mensaje: 'Libro no encontrado'})
+        }
+
+        return res.status(200).json({res:libros[0]})    
     } catch (e) {
         console.error(e)
         res.status(500).json({mensaje: 'Error en el servidor'})
--- routes/librosRoutes.js
+++ routes/librosRoutes.js
@@ -7,5 +7,8 @@ const libroController = require('../controllers/libroController')
 //Rutas para obtenener y crear libros
 router.post('/', libroController.createLibro);
 router.get('/', libroController.getLibro);
+router.get('/:id', libroController.getLibrobyId);
+//router.put('/:id', libroController.editLibro);
+//router.delete('/:id', libroController.deletLibro);
 
 module.exports = router;
